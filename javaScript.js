d2e34c7545afe06a6254a5b02a69ade4b1b1b513
Armandus <sasl09@outlook.com>
2020-02-10 11:37:51 -0600

    Played with JS buttons and events

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,31 @@
-function createAlert() {
-  alert("This is box 1");
-}
+var boxNum = 0;
+function createAlert1() {
+    boxNum = 1;
+    alert("This is box: " + boxNum);
+
+};
+
+function createAlert2() {
+    boxNum = 2;
+    alert("This is box: " + boxNum);
+};
+
+function createAlert3() {
+    boxNum = 3;
+    alert("This is box: " + boxNum);
+};
+
+function createAlert4() {
+    boxNum = 4;
+    alert("This is box: " + boxNum);
+};
+
+function createAlert5() {
+    boxNum = 5;
+    alert("This is box: " + boxNum);
+};
+
+function createAlert6() {
+    boxNum = 6;
+    alert("This is box: " + boxNum);
+};
