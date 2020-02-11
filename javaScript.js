d229e31b7264103e666da5fc94595a2bf5308ebd
Armandus <sasl09@outlook.com>
2020-02-11 12:24:08 -0600

    Played with more JS buttons and added a 'hire me' section

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,22 @@
+// function getIt() {
+//   var x = document.getElementById("someId");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+//
+//
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
 var boxNum = 0;
 function createAlert1() {
     boxNum = 1;
