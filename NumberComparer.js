ae37058c4ebe87de80d3050fcc6eec8c44ec86fc
Armandus <sasl09@outlook.com>
2020-02-11 20:53:39 -0600

    Added ternary operators to same JS previous code

--- NumberComparer.js
+++ NumberComparer.js
@@ -10,3 +10,11 @@ function determiner(x,y) {
   }
 };
 determiner(2,3)
+
+// This is the same as above, but shorter and cooler...
+// var askFor = parseInt(prompt("Enter a number..."))
+// var askFor2 = parseInt(prompt("Enter a number..."))
+// function determiner(x,y) {
+//   var determine =(x > y ? console.log(x + " is bigger than: " + y): y > x ? console.log(y + " is bigger than: " + x) : console.log(x + " equals: " + y));
+//  };
+// determiner(askFor,askFor2)
